7fc780adeafdd6bbb6eae8579069dbb85494dad0
Generalkhun <khun2551@hotmail.com>
2021-10-02 15:27:05 +0700

    Add saving villager data type

--- helpers/utils/mapRequestBodyAddVillagerFormState.ts
+++ helpers/utils/mapRequestBodyAddVillagerFormState.ts
@@ -1,14 +1,17 @@
-export const mapRequestBodyAddVillagerFormState = (addVillagerFormstate:any) => {
+import { get } from "lodash"
+import { VillagerToAddObject } from "../../type"
+
+export const mapRequestBodyAddVillagerFormState = (addVillagerFormstate: VillagerToAddObject) => {
     return {
-        HOME_ID:1,
-        HOUSE_REPRESENTATIVES_NAME:'test', 
-        HOUSE_LOCATION_LAT:'"13.68391272544513"',
-        HOUSE_LOCATION_LNG:'"100.4770417040168"',
-        CONTACT_NUMBER:'"333"',
-        FAMILY_SIZE:10,
-        ADDR_DESCRIPTION:'' ,
-        ITEM_RECIEVED:'""',
-        IMG_URL:'https://www.step1990.com/wp-content/uploads/2012/10/new.png'
+        HOME_ID: get(addVillagerFormstate, 'homeId'),
+        HOUSE_REPRESENTATIVES_NAME: get(addVillagerFormstate, 'homeRepresentativesName'),
+        HOUSE_LOCATION_LAT: get(addVillagerFormstate, 'homeLocation')[0],
+        HOUSE_LOCATION_LNG: get(addVillagerFormstate, 'homeLocation')[1],
+        CONTACT_NUMBER: get(addVillagerFormstate, 'homeRepresentativesContactNum'),
+        FAMILY_SIZE: get(addVillagerFormstate, 'numberOfFamilyMember'),
+        ADDR_DESCRIPTION: get(addVillagerFormstate, 'addressAdditionalDescription'),
+        ITEM_RECIEVED: get(addVillagerFormstate, 'isItemRecieved'),
+        IMG_URL: get(addVillagerFormstate, 'homeRepresentativesImg')
     }
 
 }
