2afb4a2f18507b6fda373ecc89ec0702c28c3431
generalkhun <khun2551@hotmail.com>
2022-12-24 12:03:55 +0700

    d

--- components/Layouts/index.tsx
+++ components/Layouts/index.tsx
@@ -10,27 +10,32 @@ const Layout: React.FC = ({ children }) => {
     const [isShowDesktopOnlyScreen, setIsShowDesktopOnlyScreen] = useState<boolean>(false)
     useEffect(() => {
         if (width < 1280) {
-            setIsShowDesktopOnlyScreen(true)
+            setIsShowDesktopOnlyScreen(true);
         }
+        setIsShowDesktopOnlyScreen(false);
     }, [width])
     return (
         <div>
-            {isShowDesktopOnlyScreen ? <div>
-                {/* render nav bar only when logged in */}
-                {isLogin ? <NavBar /> : <></>}
-                <div className={styles.bodyWrapper}>
-                    {children}
+            {isShowDesktopOnlyScreen ?
+                <div style={{
+                    height: '100vh',
+                    backgroundColor: 'white',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    gap: '20px'
+                }}>
+                    <img src="https://img.icons8.com/ios/50/null/imac.png" /><span>This app is currently available on desktop only</span>
                 </div>
-            </div> : <div style={{
-                height: '100vh',
-                backgroundColor: 'white',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                gap: '20px'
-            }}>
-                <img src="https://img.icons8.com/ios/50/null/imac.png" /><span>This app is currently available on desktop only</span>
-            </div>}
+                :
+                <div>
+                    {/* render nav bar only when logged in */}
+                    {isLogin ? <NavBar /> : <></>}
+                    <div className={styles.bodyWrapper}>
+                        {children}
+                    </div>
+                </div>
+            }
         </div>
 
     )
