0ff3f5abfccafca7dd17ddeb60c344cef2bbdf33
generalkhun <khun2551@hotmail.com>
2022-12-24 12:30:26 +0700

    d

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,17 +7,7 @@ import { GoogleSheetDataProvider } from '../contextProviders/GoogleSheetContextP
 import { DisplayVillagerDataProvider } from '../contextProviders/DisplayingVillagerDataContextProvider'
 import { ThemeProvider, createTheme } from '@material-ui/core/styles'
 import { LoginContextProvider } from '../contextProviders/LoginContextProvider'
-import { useWindowSize } from '../hooks/useWindowResize'
 function MyApp({ Component, pageProps }: AppProps) {
-  const [width] = useWindowSize()
-  const [isShowDesktopOnlyScreen, setIsShowDesktopOnlyScreen] = useState<boolean>(false)
-  useEffect(() => {
-    if (width < 1280) {
-      setIsShowDesktopOnlyScreen(true);
-    }
-    setIsShowDesktopOnlyScreen(false);
-  }, [width])
-
   const theme = createTheme({
     typography: {
       "fontFamily": `"Kanit","Roboto", "Helvetica", "Arial", sans-serif`,
@@ -32,25 +22,12 @@ function MyApp({ Component, pageProps }: AppProps) {
       <LoginContextProvider>
         <GoogleSheetDataProvider>
           <DisplayVillagerDataProvider>
-            {isShowDesktopOnlyScreen ?
-              <div style={{
-                height: '100vh',
-                backgroundColor: 'white',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                gap: '20px'
-              }}>
-                <img src="https://img.icons8.com/ios/50/null/imac.png" /><span>This app is currently available on desktop only</span>
-              </div>
-              :
-              <Layouts>
-                <Head>
-                  <link href='https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css' rel='stylesheet' />
-                </Head>
-                <Component {...pageProps} />
-              </Layouts>
-            }
+            <Layouts>
+              <Head>
+                <link href='https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css' rel='stylesheet' />
+              </Head>
+              <Component {...pageProps} />
+            </Layouts>
           </DisplayVillagerDataProvider>
         </GoogleSheetDataProvider>
       </LoginContextProvider>
